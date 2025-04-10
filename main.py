43413aede847849d527a2dbbbb8130e3043bf67b
dragon6810 <35243479+dragon6810@users.noreply.github.com>
2025-04-10 17:46:54 -0400

    drawing circles for select particles

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ class GerstnerWave(Scene):
         nx = 32
         ny = 8
         dotspace = 1/2
+        circlespace = 4
+        circley = ny - 1
 
         tracker = ValueTracker(0)
         self.add(tracker)
-        dots = VGroup()
+        circledots = VGroup()
+        alonedots = VGroup()
 
         wavelen = 8
         height = 1
@@ -33,9 +36,14 @@ class GerstnerWave(Scene):
                     d.move_to([offs[0] + px, offs[1] + py, 0])
 
                 dot.add_updater(updater)
-                dots.add(dot)
 
-        self.add(dots)
+                if(ix % circlespace != 0 or iy != circley):
+                    alonedots.add(dot)
+                else:
+                    circledots.add(dot)
+
+        self.add(circledots)
+        self.add(alonedots)
 
         k = 2 * np.pi / wavelen
         g = 9.8
@@ -44,25 +52,43 @@ class GerstnerWave(Scene):
 
         def update_tracker(mob, dt):
             current = mob.get_value()
-            if current < T:
-                mob.set_value(min(current + dt, T))
+            mob.set_value(current + dt)
 
         tracker.add_updater(update_tracker)
 
         self.wait(T)
+
+        circles = VGroup()
+
+        for ix in range(0, nx, circlespace):
+            iy = circley
+            px = (ix - nx / 2) * dotspace
+            py = (iy - ny) * dotspace
+            offs = wavegetoffset(px, py, 0, wavelen, height)
+            
+            circle = Circle(radius=np.sqrt(offs[0]**2+offs[1]**2))
+            circle.move_to([px, py, 0])
+            circle.set_fill(PINK, opacity=0.2)
+            circles.add(circle)
+
+        self.play(Create(circles, run_time=1.0), *[dot.animate.set_opacity(0.2) for dot in alonedots])
+
+        self.wait(T*2-1.0)
+
+        self.play(Uncreate(circles, run_time=1.0), *[dot.animate.set_opacity(1.0) for dot in alonedots])
+
+        self.wait(T-1.0)
+
         tracker.remove_updater(update_tracker)
-
-        #self.play(tracker.animate.set_value(T), run_time=T, rate_func=linear)
-
-        ix = nx / 2
-        iy = ny / 2
-        px = (ix - nx / 2) * dotspace
-        py = (iy - ny) * dotspace
 
 def wavegetoffset(a, b, t, wavelength, A=1):
     k = 2 * np.pi / wavelength
     g = 9.8
     c = np.sqrt(g / k)
-    x =  A * np.exp(-k * abs(b)) * np.sin(k * (a + c * t))
-    y = -A * np.exp(-k * abs(b)) * np.cos(k * (a + c * t))
+    radius = A * np.exp(k * b)
+    theta = k * (a + c * t)
+
+    x =  radius * np.sin(theta)
+    y = -radius * np.cos(theta)
+    
     return [x, y]
